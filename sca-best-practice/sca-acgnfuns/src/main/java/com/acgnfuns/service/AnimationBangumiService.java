f60f93eaccf936533e45b2f18fc4fd91bc752b4e
sppsun <sppsun@gmail.com>
2019-06-27 06:48:08 +0900

    first commit

---
+++ sca-best-practice/sca-acgnfuns/src/main/java/com/acgnfuns/service/AnimationBangumiService.java
@@ -0,0 +1,10 @@
+package com.acgnfuns.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AnimationBangumiService {
+    public void test() {
+        System.out.println("Hello World");
+    }
+}
